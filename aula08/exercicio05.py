3575b6b2fce2106942a95971a41f97e65273d2a9
markimvso <74935300+marcovinicius1@users.noreply.github.com>
2024-04-15 18:23:34 -0300

    Update exercicio05.py

--- aula08/exercicio05.py
+++ aula08/exercicio05.py
@@ -1,1 +1,18 @@
-# TIRAR DUVIDA COM O PROFESSOR
+n = int(input("Digite o número de termos da série de Fibonacci: "))
+
+if n <= 0:
+  print("O número de termos deve ser maior que zero.")
+else:
+
+  termo1 = 0
+  termo2 = 1
+
+  print("Série de Fibonacci:")
+  print(termo1)
+  print(termo2)
+
+  for i in range(2, n):
+    proximo_termo = termo1 + termo2
+    print(proximo_termo)
+    termo1 = termo2
+    termo2 = proximo_termo
